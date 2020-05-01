f6782470ab669c0ca4f89c06eb68d4450df3c084
alex-babich <ababich.dev@gmail.com>
2020-05-01 15:52:56 +0300

    1) updated style and content for pages main and checkout; 2) addednew fonts, images (svg), styles;

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -12,19 +12,33 @@
     </head>
     <body>
         <div class="df-page-checkout">
-            <section class="df-grey-bg df-pb-50 df-pt-50">
+            <section class="df-grey-bg">
                 <div class="df-container">
                     <div class="df-row df-justify-center df-align-center">
-                        <h2 class="df-page-title">Checkout</h2>
+                        <h2 class="df-page-title df-mb-40">Checkout</h2>
                     </div>
                 </div>
                 <div class="df-order-details">
                     <div class="df-order-title">
                         <p>Order details</p>
                     </div>
-                    <div class="df-order-info">
+                    <div class="df-order-info df-flex df-justify-around df-pt-30 df-pb-30">
                         <div class="df-product-image">
                             @svg('box6.svg')
+                        </div>
+                        <div class="df-product-price-info df-flex">
+                            <div class="df-product-price-item df-flex df-direction-row df-justify-between">
+                                <p class="df-product-title">6 boxes</p>
+                                <p class="df-crossed-text">$420.00</p>
+                            </div>
+                            <div class="df-product-price-item df-flex df-direction-row df-justify-between">
+                                <p>Discount <b>-46%</b></p>
+                                <p class="df-bold df-text-red">- $151.00</p>
+                            </div>
+                            <div class="df-product-price-item df-order-total df-flex df-direction-row df-justify-between">
+                                <h4>Total</h4>
+                                <h4 class="df-bold">$269.00</h4>
+                            </div>
                         </div>
                     </div>
 
@@ -32,9 +46,35 @@
                         <p>SHIPPING DETAILS</p>
                     </div>
 
-                    <div class="df-order-contact">
-                        
-                    </div>
+                    <form class="df-order-contact">
+                        <div class="df-flex df-direction-column df-order-contact-item">
+                            <label class="df-mb-10">Full name</label>
+                            <input type="text" placeholder="Enter here" />
+                        </div>
+                        <div class="df-order-contact-item">
+                            <label class="df-mb-10">Street address</label>
+                            <input type="text" placeholder="Enter here" />
+                        </div>
+                        <div class="df-order-contact-item">
+                            <label class="df-mb-10">Town / city</label>
+                            <input type="text" placeholder="Enter here" />
+                        </div>
+                        <div class="df-order-contact-item">
+                            <label class="df-mb-10">State</label>
+                            <input type="text" placeholder="Enter here" />
+                        </div>
+                        <div class="df-order-contact-item">
+                            <label class="df-mb-10">Zip code</label>
+                            <input type="text" placeholder="Enter here" />
+                        </div>
+                        <div class="df-order-contact-item">
+                            <label class="df-mb-10">Phone number</label>
+                            <input type="text" placeholder="Enter here" />
+                        </div>
+                        <div class="df-order-contact-item df-justify-center">
+                            <button type="submit" class="df-button df-button-confirm">Confirm</button>
+                        </div>
+                    </form>
                 </div>
             </section>
         </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,8 +14,8 @@
         <div class="df-page-welcome">
             <section>
                 <div class="df-container">
-                    <!-- class df-pass-first-step and df-pass-second-step is showing progress-->
-                    <div class="df-row df-justify-between df-steps-nav df-pass-first-step">
+                    <!-- is class df-pass-first-step and df-pass-second-step is showing progress-->
+                    <div class="df-row df-justify-between df-steps-nav df-pass-second-step">
                         <div class="df-steps-item df-active">
                             <span class="df-steps-point"></span>
                             <p class="df-steps-title">Order submitted</p>
@@ -32,10 +32,10 @@
                     <div class="df-row df-justify-center df-direction-column df-align-center">
                         <h2 class="df-page-title">Wait!</h2>
                         <h4 class="df-page-subtitle">Your order is not complete.</h4>
-                        <h4 class="df-page-subtitle">We have a <span class="df-text-blue df-underline">special offer for you</span>.</h4>
+                        <h4 class="df-page-subtitle df-mobile-text">We have a <span class="df-text-blue df-underline">special offer for you</span>.</h4>
                     </div>
                     <div class="df-row df-justify-center">
-                        <p class="df-mt-40 df-page-description">You want to achieve your target weight as soon as possible, right? Well, fasting requires patience. However, there’s something you can do <b class="df-text-purple">RIGHT NOW</b> to make that wait a little shorter<br/><i>(and 10x times more enjoyable!)</i></p>
+                        <p class="df-page-description">You want to achieve your target weight as soon as possible, right? Well, fasting requires patience. However, there’s something you can do <b class="df-text-purple">RIGHT NOW</b> to make that wait a little shorter<br/><i>(and 10x times more enjoyable!)</i></p>
                     </div>
                     <div class="df-row df-justify-around df-align-center df-order-points">
                         <div class="df-flex df-align-center">
@@ -57,12 +57,12 @@
                             <h5>Defeat hunger pans</h5>
                         </div>
                     </div>
-                    <div class="df-row df-justify-center df-align-center df-mb-40">
+                    <div class="df-row df-justify-center df-align-center df-mb-40 df-mobile-text">
                         <h5>with a <span class="df-text-blue df-underline">100% natural, science-based</span> solution</h5>
                     </div>
                     <div class="df-row df-justify-center df-align-center df-mb-30">
                         <div class="df-image">
-                            @svg('house.svg', 'df-house')
+                            @svg('hugebox.svg', 'df-hugebox')
                         </div>
                     </div>
 
@@ -92,7 +92,7 @@
 
                     <div class="df-row df-justify-center df-align-center df-text-block df-mb-50">
                         <h5>Just use one glass of this hunger-crushing solution and you will be amazed at the benefits it provides.</h5>
-                        <h5>There is <span class="df-text-purple">NO SUGAR</span> and just <span class="df-text-purple">3 CALORIES</span> per serving.</h5>
+                        <h5 class="df-mobile-text ">There is <span class="df-text-purple">NO SUGAR</span> and just <span class="df-text-purple">3 CALORIES</span> per serving.</h5>
                         <h4 class="df-text-blue">It will not break your fast!</h4>
                     </div>
                 </div>
@@ -102,30 +102,81 @@
                 <div class="df-container">
                     <div class="df-row df-justify-center df-align-center df-text-block df-mb-50 df-offer-text">
                         <h5>This is an exclusive offer</h5>
-                        <h5>On this page only</h5>
+                        <h5 class="df-uppercase-text">On this page only</h5>
                     </div>
 
-                    <div class="df-row df-justify-center df-direction-column df-align-center df-mb-50">
+                    <div class="df-row df-justify-center df-direction-column df-align-center df-mb-50 df-second-title">
                         <h2 class="df-page-title">Get a <span class="df-text-blue">30-pack BOX</span> of natural</h2>
                         <h2 class="df-page-title df-text-purple">Essential Fiber Complex</h2>
                         <p class="df-bold">with a detailed guide formulated by nutrition experts</p>
-                        <a href="" tabindex="" class="df-link df-link-purple df-mt-30">FULL 30-DAY SATISFACTION GUARANTEE</a>
+                        <a href="" class="df-link df-link-purple df-mt-30">FULL 30-DAY SATISFACTION GUARANTEE</a>
                     </div>
 
                     <div class="df-row df-justify-center df-align-center df-mb-30">
                         <div class="df-image">
-                            @svg('house.svg', 'df-house')
+                            @svg('hugebox.svg', 'df-hugebox')
                         </div>
                     </div>
 
                     <div class="df-row df-justify-center df-align-center df-mb-40 df-product-list">
-                        <div class="df-product-item df-most-popular">
-                            <div class="">
-                                MOST POPULAR
+                        <div class="df-product-wrapper df-most-popular">
+                            <div class="df-most-popular-title">
+                                <p>
+                                    <span>@svg('star.svg', 'df-star')</span>
+                                    MOST POPULAR
+                                </p>
+                            </div>
+                            <div class="df-product-item">
+                                <p class="df-poduct-title">6 months *</p>
+                                <p class="df-safe-mony">SAVE $150.96</p>
+                                <div class="df-product-image">
+                                    @svg('box6.svg', 'df-product')
+                                </div>
+                                <div class="df-product-price">
+                                    <p>$<b>269</b> / 6 boxes</p>
+                                </div>
+                                <div class="df-product-info">
+                                    <p><span>$44.83 per box</span><span>$1.49 per serving</span></p>
+                                    <p>180 servings</p>
+                                    <p class="df-text-important">6 boxes will be delivered at once</p>
+                                </div>
+                                <button class="df-button df-buy-button">BUY NOW</button>
                             </div>
                         </div>
-                        <div class="df-product-item"></div>
-                        <div class="df-product-item"></div>
+                        <div class="df-product-wrapper">
+                            <div class="df-product-item">
+                                <p class="df-poduct-title">3 months *</p>
+                                <p class="df-safe-mony">SAVE $60.97</p>
+                                <div class="df-product-image">
+                                    @svg('box3.svg', 'df-product')
+                                </div>
+                                <div class="df-product-price">
+                                    <p>$<b>149</b> / 3 boxes</p>
+                                </div>
+                                <div class="df-product-info">
+                                    <p><span>$49.66 per box</span><span>$1.65 per serving</span></p>
+                                    <p>90 servings</p>
+                                    <p class="df-text-important">3 boxes will be delivered at once</p>
+                                </div>
+                                <button class="df-button df-buy-button">BUY NOW</button>
+                            </div>
+                        </div>
+                        <div class="df-product-wrapper">
+                            <div class="df-product-item">
+                                <p class="df-poduct-title">1 months</p>
+                                <div class="df-product-image">
+                                    @svg('box1.svg', 'df-product')
+                                </div>
+                                <div class="df-product-price">
+                                    <p>$<b>69.99</b> / 1 box</p>
+                                </div>
+                                <div class="df-product-info">
+                                    <p><span>$2.33 per serving</span></p>
+                                    <p>30 servings</p>
+                                </div>
+                                <button class="df-button df-buy-button">BUY NOW</button>
+                            </div>
+                        </div>
                     </div>
 
                     <div class="df-row df-justify-center df-align-center df-mb-30">
